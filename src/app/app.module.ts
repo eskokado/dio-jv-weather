b3c7c63188564fdee6359ab11281f1b3bfd7d9e9
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-18 07:26:36 -0300

    Criando Pages e Components (Home e Bookmarks) e mapeando as routas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+import { HomeModule } from './pages/home/home.module';
+import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    BookmarksModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
